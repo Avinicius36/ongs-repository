ec5a25d9afdde54887e1ee91505dbf2076d10520
Antonio Vinicius Rocha Araujo <CIANDT\viniciusrocha@lnb028755bhz.ciandt.global>
2021-09-13 13:28:47 -0300

    Meu primeiro commit para o serviço de ongs

---
+++ src/main/java/com/webdesignerportalimigrante/ongs/domain/Ongs.java
@@ -0,0 +1,83 @@
+package com.webdesignerportalimigrante.ongs.domain;
+
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+public class Ongs {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @NotBlank(message = "Name is mandatory")
+    private String name;
+
+    @NotBlank(message = "Email is mandatory")
+    private String email;
+
+    public Ongs() {
+    }
+
+    public Ongs(long id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Ongs{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Ongs ongs = (Ongs) o;
+        return id == ongs.id && Objects.equals(name, ongs.name) && Objects.equals(email, ongs.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, email);
+    }
+
+}
